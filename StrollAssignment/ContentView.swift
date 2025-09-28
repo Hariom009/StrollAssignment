fbd4998228b1f9ba7c0b12660a6b37a93ba09057
Hariom <hariomconnects03@gmail.com>
2025-09-28 17:16:09 +0530

    Initial Commit

---
+++ StrollAssignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StrollAssignment
+//
+//  Created by Hari's Mac on 28.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StrollAssignment/StrollAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrollAssignmentApp.swift
+//  StrollAssignment
+//
+//  Created by Hari's Mac on 28.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct StrollAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
